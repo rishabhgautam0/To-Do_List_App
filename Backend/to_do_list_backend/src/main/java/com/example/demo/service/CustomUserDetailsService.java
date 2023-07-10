589942c27cdea77795843db5ed0c6a8112d5786e
unknown <kumarrishabhgautam0@gmail.com>
2023-07-10 11:28:09 +0530

    fixed errror of dependency injection of UserDetailsService

--- Backend/to_do_list_backend/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ Backend/to_do_list_backend/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepo;
 
+
+@Service
+@Transactional
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
