aa3fefaa7b225723ebfd41e5dad9860c29243892
unknown <kumarrishabhgautam0@gmail.com>
2023-07-17 03:29:37 +0530

    everything working in the localhost

--- Frontend/to_do_list_frontend/src/common/constants.js
+++ Frontend/to_do_list_frontend/src/common/constants.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = "https://to-do-list.ap-south-1.elasticbeanstalk.com";
+// export const BASE_API_URL = "https://to-do-list.ap-south-1.elasticbeanstalk.com";
+export const BASE_API_URL = "http://localhost:8080";
