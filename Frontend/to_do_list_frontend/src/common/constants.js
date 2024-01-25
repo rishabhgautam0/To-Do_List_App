229f7396179a0f3731868b4407e37ca828f2f2f0
unknown <kumarrishabhgautam0@gmail.com>
2024-01-26 00:00:27 +0530

    updated to run locally

--- Frontend/to_do_list_frontend/src/common/constants.js
+++ Frontend/to_do_list_frontend/src/common/constants.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "http://todo-list.ap-south-1.elasticbeanstalk.com";
-// export const BASE_API_URL = "http://localhost:8080";
+// export const BASE_API_URL = "http://todo-list.ap-south-1.elasticbeanstalk.com";
+export const BASE_API_URL = "http://localhost:8080";
